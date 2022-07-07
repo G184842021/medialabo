5463a138536fa08f7bd1cdddd80f5a8e48ce3f99
G184842021 <218484@st.takushoku-u.ac.jp>
2022-07-07 15:29:31 +0900

    第５回イベント処理

--- kazuate.js
+++ kazuate.js
@@ -5,21 +5,23 @@ console.log('答え: ' + kotae);      // デバッグ用
 // 入力回数（予想回数）
 let kaisu = 0;
 let n = 0;
+let yoso;
 // 予想を4回実行する
+let b = document.querySelector('#print');
+b.addEventListener('click', hantei);
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
-
-hantei();
-hantei();
-hantei();
-hantei();
 // ボタンを押した後の処理をする関数 hantei() の定義
 
 function hantei() {
+    let i = document.querySelector('input[name="answer"]');
+	yoso = i.value;
+    yoso = Number(yoso);
+
     let a = document.querySelector('span#t');
     let b = document.querySelector('span#kaisu');
     kaisu = kaisu + 1;
     n = n + 1;
-    let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
+         // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
